cf43464c0e1d4ff800250fa8e231f262533bd2af
hoanprd <99530243+hoanprd@users.noreply.github.com>
2024-10-04 13:33:34 +0700

    Add bullet script, update enemy prefab and add bullet to MainScene

---
+++ Mana of Alchemist/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attribute")]
+    [SerializeField] private float bulletSpeed = 5f;
+    private Transform target;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
