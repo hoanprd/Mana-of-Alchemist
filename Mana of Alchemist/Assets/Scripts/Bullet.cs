38c53d6069d1ac28f5c8dfd40c036c469c2f51ac
hoanprd <tinhprd@gmail.com>
2024-10-06 15:42:32 +0700

    Complete Bullet script, add Health script for Enemy, change MainScene in inspector, change Turret script to spawn bullet, add new Layer, change game Physics2D setting

--- Mana of Alchemist/Assets/Scripts/Bullet.cs
+++ Mana of Alchemist/Assets/Scripts/Bullet.cs
@@ -9,11 +9,28 @@ public class Bullet : MonoBehaviour
 
     [Header("Attribute")]
     [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private int bulletDamage = 1;
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
     private Transform target;
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        if (!target) return;
+
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.velocity = direction * bulletSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Health>().TakeDamage(bulletDamage);
+        Destroy(gameObject);
     }
 }
--- Mana of Alchemist/Assets/Scripts/Turret.cs
+++ Mana of Alchemist/Assets/Scripts/Turret.cs
@@ -47,7 +47,9 @@ public class Turret : MonoBehaviour
 
     private void Shoot()
     {
-        Debug.Log("Shoot");
+        GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+        Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+        bulletScript.SetTarget(target);
     }
 
     private void FindTarget()
